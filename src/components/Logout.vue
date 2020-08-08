870538e43f3024ab08fa64efe61d275e428574ab
Iana Spivac <62013515+magumi@users.noreply.github.com>
2020-08-08 16:09:20 +0300

    initial commit

---
+++ src/components/Logout.vue
@@ -0,0 +1,18 @@
+
+<template>
+  <button @click="signOut()">Sign out</button>
+</template>
+<script>
+export default {
+  name: "Logout",
+  data: function() {
+    return {};
+  },
+  methods: {
+    signOut() {
+      localStorage.clear();
+      this.$router.push({ path: `/landing-page` }); //denumirea path-ului de schimbat
+    }
+  }
+};
+</script>
