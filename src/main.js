82614541e6a8ab4db14fecb28212e3d14077f8cc
Iana Spivac <62013515+magumi@users.noreply.github.com>
2020-08-08 15:01:32 +0300

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 import Vue from "vue";
+import VueRouter from "vue-router";
 import App from "./App.vue";
+import Login from "./Login.vue";
+import Register from "./Register.vue";
 
 Vue.config.productionTip = false;
 
+Vue.use(VueRouter);
+const routes = [
+  { path: "/login", component: Login },
+  { path: "/register", component: Register }
+];
+
+const router = new VueRouter({
+  mode: "history", // ne ajuta sa avem URL mai curate (fara #)
+  routes: routes
+});
 new Vue({
-  render: h => h(App)
+  render: (h) => h(App),
+  router
 }).$mount("#app");
