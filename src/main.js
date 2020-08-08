1f37b13051c29e50280ec42fd00bc4cca85ab3e3
agressive-nicoleta <53252698+agressive-nicoleta@users.noreply.github.com>
2020-08-08 15:36:38 +0300

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
 import Vue from "vue";
 import App from "./App.vue";
+import HelloWorld from "./components/HelloWorld";
+import VueRouter from "vue-router";
+import Dashboard from "./Dashboard";
+
+Vue.use(VueRouter);
 
 Vue.config.productionTip = false;
 
+const routes = [
+  { path: "/", component: HelloWorld },
+  { path: "/dashboard", component: Dashboard },
+  { path: "/notes/:id", component: Notes}
+];
+
+const router = new VueRouter({
+  mode: "history",
+  routes: routes
+});
+
 new Vue({
-  render: h => h(App)
+  render: (h) => h(App),
+  router
 }).$mount("#app");
