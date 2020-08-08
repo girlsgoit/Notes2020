d085b9e274fe4d98999fa99a29c9dbf576cad68c
Iuliana0109 <68803808+Iuliana0109@users.noreply.github.com>
2020-08-08 15:12:21 +0300

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from "vue";
+import HelloWorld from "./components/HelloWorld";
 import App from "./App.vue";
+import VueRouter from "vue-router";
+Vue.use(VueRouter);
 
 Vue.config.productionTip = false;
 
+const routes = [{ path: "/", component: HelloWorld }];
+
+const router = new VueRouter({
+  mode: "history",
+  routes: routes
+});
+
 new Vue({
-  render: h => h(App)
+  render: (h) => h(App),
+  router
 }).$mount("#app");
