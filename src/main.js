eea6ec98d9299eb00cb9d1512ca12b5267623e42
Iana Spivac <62013515+magumi@users.noreply.github.com>
2020-08-08 16:31:15 +0300

    Merged 19 and 20 Sincronizarea taskurilor.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import HelloWorld from "./components/HelloWorld.vue";
 import App from "./App.vue";
 import Login from "./Login.vue";
 import Register from "./Register.vue";
@@ -7,13 +8,12 @@ import Register from "./Register.vue";
 Vue.config.productionTip = false;
 
 Vue.use(VueRouter);
-const router = new VueRouter({
-  mode: "history", // ne ajuta sa avem URL mai curate (fara #)
-  routes: routes
-});
 
-const routes = [{ path: "/", component: HelloWorld }, { path: "/login", component: Login },
-  { path: "/register", component: Register }];
+const routes = [
+  { path: "/", component: HelloWorld },
+  { path: "/login", component: Login },
+  { path: "/register", component: Register }
+];
 
 const router = new VueRouter({
   mode: "history",
