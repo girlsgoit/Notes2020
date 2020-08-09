c46b711be4d480e7fd77303f78ee925891396aec
danielverejan <daniel.verejan@gmail.com>
2020-08-09 16:41:03 +0300

    Fix login

--- src/components/LogIn.vue
+++ src/components/LogIn.vue
@@ -3,9 +3,19 @@
     <img
       class="logo"
       src="https://s3-alpha-sig.figma.com/img/eb24/ca1c/6b46f3629797526e406626fc13be7290?Expires=1597622400&Signature=SX5MRkeXwQWPZ1MpDbbd-niUIsiEgrQk7BsW8iwj1RVj~QxlTWHRqw66e5AkZWPD-DElxe1bgVSuU4NEykSlTaa6N-YwQuGEt~NKPHYd-YBQDATFV5Y-JwO8dMBH~gQ6xTZQdYpOssiZZJgOLgXnUChwLU-rO4RNAofeYrcU4n11ybICxJOlXrBTk-YqDOhl1ITDghO37-s2GH~P-jjlNzGUOXiomRtsqAMF5SPEK8yykxW59CoL9krPH5UeFngyNN3oAQN5pGp8~m2mJOKnX9xuRUE2pckqfkhcdB7waVPvwO8Vn1dYBvGStWdCd~hcMRP0z7aOObVuHrikcw3qRg__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
-    >
-    <input type="text" name="username" placeholder="Username" v-model="input.username">
-    <input type="password" name="password" v-model="input.password" placeholder="Password">
+    />
+    <input
+      type="text"
+      name="username"
+      placeholder="Username"
+      v-model="input.username"
+    />
+    <input
+      type="password"
+      name="password"
+      v-model="input.password"
+      placeholder="Password"
+    />
     <div class="buttons">
       <a class="buton-login" v-on:click="login()">LOGIN</a>
       <a class="buton-register" v-on:click="goToRegister()">REGISTER</a>
@@ -23,11 +33,17 @@ export default {
         username: "",
         password: ""
       },
-      NOTES_AUTH: null
+      NOTES_AUTH: null,
+      loginInProgress: false
     };
   },
   methods: {
     login() {
+      if (this.loginInProgress) {
+        return;
+      }
+      this.loginInProgress = true;
+      const that = this;
       if (this.input.username !== "" && this.input.password !== "") {
         const data = {
           username: this.input.username,
@@ -37,25 +53,28 @@ export default {
         // password: puper
         axios
           .post("https://notes-api.girlsgoit.org/users/login/", data)
-          .then(function(response) {
+          .then(function (response) {
             if (response.status === 200) {
               localStorage.setItem("NOTES_AUTH", response.data.token);
               localStorage.setItem("username", response.data.username);
-              //this.$router.push({ path: "/dashboard" }); sync
+              that.$router.push({ path: "/dashboard" });
+              sync;
             }
 
             console.log(response);
           })
-          .catch(function(error) {
+          .catch(function (error) {
             alert("Invalid username or password.");
             console.log(error.response);
+            that.loginInProgress = false;
           });
       } else {
         alert("A username and password must be present.");
+        that.loginInProgress = false;
       }
     },
-    goToRegister(){
-      //this.$router.push({ path: "/register" });
+    goToRegister() {
+      this.$router.push({ path: "/register" });
     }
   }
 };
@@ -133,4 +152,3 @@ input {
   margin-left: 27px;
 }
 </style>
-
