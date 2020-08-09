42ccf9ea3de274a27aef2d98a899e1646e149f73
AlexandraMihalevschi <68810700+AlexandraMihalevschi@users.noreply.github.com>
2020-08-09 15:01:48 +0300

    initial commit

--- src/main.js
+++ src/main.js
@@ -4,5 +4,27 @@ import App from "./App.vue";
 Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App)
+  render: (h) => h(App)
+}).$mount("#app");
+
+Vue.config.productionTip = false;
+
+Vue.use(VueRouter);
+
+const routes = [
+  { path: "/", component: HelloWorld },
+  { path: "/login", component: Login },
+  { path: "/register", component: Register },
+  { path: "/dashboard", component: Dashboard },
+  { path: "/notes/:id", component: Notes }
+];
+
+const router = new VueRouter({
+  mode: "history",
+  routes: routes
+});
+
+new Vue({
+  render: (h) => h(App),
+  router
 }).$mount("#app");
