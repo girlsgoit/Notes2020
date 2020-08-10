92ecb65e285afc30ead88396ad23ef74e2da8540
agressive-nicoleta <53252698+agressive-nicoleta@users.noreply.github.com>
2020-08-10 10:53:31 +0300

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <div id="app">
-    
+    <Header></Header>
     <router-view></router-view>
-
-    
   </div>
 </template>
 
 <script>
+import Header from "./components/Header";
+
 export default {
   name: "App",
-  components: {}
+  components: {
+    Header
+  }
 };
 </script>
 
@@ -21,6 +23,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
