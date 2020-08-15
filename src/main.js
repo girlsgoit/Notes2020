5ae2ab5c9c8a2e8058c413bdb2832a4cbb93f9a5
Marc Graciov <mark.graciov@gmail.com>
2020-08-15 23:55:08 +0300

    Login guard and default page

--- src/main.js
+++ src/main.js
@@ -10,35 +10,50 @@ import Axios from "axios";
 
 Vue.config.productionTip = false;
 
-Axios.interceptors.request.use(
-  function (config) {
-    const token = localStorage.getItem("NOTES_AUTH");
-
-    if (token) {
-      config.headers["Authorization"] = "Token " + token;
-    }
-
-    return config;
-  },
-  function (error) {
-    console.log(error);
-  }
-);
 Vue.use(VueRouter);
+
+function loginGuard(from, to, next) {
+  console.log("guarding!");
+  const token = localStorage.getItem("NOTES_AUTH");
+  if (token) {
+    next();
+  } else {
+    next("/login");
+  }
+}
 
 const routes = [
   { path: "/", component: Landing },
   { path: "/login", component: LogIn },
   { path: "/register", component: Register },
-  { path: "/dashboard", component: Dashboard },
-  { path: "/notes/:id", component: Notes },
-  { path: "/new-notes", component: Notes }
+  { path: "/dashboard", component: Dashboard, beforeEnter: loginGuard },
+  { path: "/notes/:id", component: Notes, beforeEnter: loginGuard },
+  { path: "/new-notes", component: Notes, beforeEnter: loginGuard },
+  { path: "*", component: Landing }
 ];
 
 const router = new VueRouter({
   mode: "history",
   routes: routes
 });
+
+
+Axios.interceptors.request.use(
+  function(config) {
+    const token = localStorage.getItem("NOTES_AUTH");
+
+    if (token) {
+      config.headers["Authorization"] = "Token " + token;
+    } else {
+      router.push("/login");
+    }
+
+    return config;
+  },
+  function(error) {
+    console.log(error);
+  }
+);
 
 new Vue({
   render: (h) => h(App),
