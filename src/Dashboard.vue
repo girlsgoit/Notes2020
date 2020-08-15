474163dd8b2a20770c65cc6a6251b0a606e22042
Cristian Petcu <waickem@gmail.com>
2020-08-16 01:05:14 +0300

    Max width for notes' images on the dashboard (#49)

--- src/Dashboard.vue
+++ src/Dashboard.vue
@@ -83,7 +83,6 @@ export default {
 @import url("https://fonts.googleapis.com/css2?family=Roboto:wght@100;300&display=swap");
 
 .cards-section {
-  max-width: 960px;
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
