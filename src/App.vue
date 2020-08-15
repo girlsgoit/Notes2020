4c896728b8d22d9e15103534c89b69e70caef273
Daniel Verejan <danielverejan@users.noreply.github.com>
2020-08-15 15:02:10 +0300

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,36 @@
 <template>
   <div id="app">
-    <Header></Header>
+    <HeaderLogIn :user="user" v-if="token"/>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-import Header from "./components/Header";
+import HeaderLogIn from "./components/HeaderLogIn";
 
 export default {
   name: "App",
   components: {
-    Header
+    HeaderLogIn
+  },
+  data() {
+    return {
+      token: "",
+      user: {}
+    };
+  },
+  created() {
+    this.token = localStorage.getItem("NOTES_AUTH");
+
+    if (this.token) {
+      this.user = {
+        username: localStorage.getItem("USER_NAME"),
+        fullName: localStorage.getItem("FULL_NAME"),
+        id: localStorage.getItem("USER_ID")
+      };
+
+      console.log(this.user);
+    }
   }
 };
 </script>
